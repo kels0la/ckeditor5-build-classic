6af407f3bdd860bc3d2d617cb9506d781fe9407c
Sam <samelsola@gmail.com>
2021-01-08 00:45:22 -0500

    praying

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ const webpack = require( 'webpack' );
 const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
 	devtool: 'source-map',
@@ -45,6 +46,9 @@ module.exports = {
 	},
 
 	plugins: [
+		new MiniCssExtractPlugin( {
+			filename: 'custom.css'
+	} ),
 		new CKEditorWebpackPlugin( {
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
